bfae1ea5b19ffb8c8789c404ae01f408590bd756
mac <mac@macs-MacBook-Air.local>
2023-01-29 13:57:14 +0200

    task 3

---
+++ task3.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+credits = pd.read_csv("./credits.csv")
+titles = pd.read_csv("./titles.csv")
+
+def task3():
+    years = titles[titles['release_year'] > 1999]
+    years_values = years[years['imdb_score'] >= 8.0]
+    years_group = years.groupby(by='release_year')['release_year'].count().sort_index()
+    years_score = years_values.groupby(by="release_year")['imdb_score'].count().sort_values()
+    dataframe = pd.DataFrame({'lab': years_group.keys(), 'val': ((years_score / years_group)*100)})
+    list_bar = plt.bar(x=dataframe["lab"], height=dataframe['val'])
+    list_bar[int(dataframe['lab'][dataframe['val'] == max(dataframe['val'])]) - 2000].set_facecolor('red')
+    plt.xlabel("Years")
+    plt.ylabel("Score")
+    plt.title("SHOW")
+    #print(years_score )
+
+task3()
+plt.show()
+
+
+
